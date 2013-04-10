6e6634eea586f6066d25edd3ae5c298a767cb36f
Fyodor Y <fygrave@o0o.nu>
2013-04-10 10:41:54 +0800

    sjunpack fixes

--- ryocrawler/wascanner.py
+++ ryocrawler/wascanner.py
@@ -12,7 +12,31 @@ import jsunpack
 
 
 class Scanner(object):
-    jsunpackopts = {"active": True}
+    class Opts:
+        rules = ""
+        rulesAscii = ""
+        active = True
+        veryverbose = True
+        verbose = True
+        outdir = "/tmp"
+        tmpdir = "/tmp"
+        currentproxy = ''
+        nojs = False
+        fasteval = False
+        redoevaltime = 10
+        htmlparse = False
+        timeout = 60
+        proxy = None
+        htmlparseconfig = ""
+        interface = None
+        urlfetch = None
+        pre = "pre.js"
+        maxruntime = 10
+        post = "post.js"
+        quiet = False
+        saveallfiles = True
+        debug = False
+    jsunpackopts = Opts()
 
     def __init__(self):
         self.yara = yara.compile("%s/yara/web.yar" % os.path.dirname(os.path.realpath(__file__)))
@@ -20,22 +44,33 @@ class Scanner(object):
     def _fil(self, s):
         return "".join(filter(lambda x: ord(x)<128, s))
 
-    def scan(self, data):
-    #    js = jsunpack.jsunpackn.jsunpack("/tmp/a", ['', data, "/tmp/a"], self.jsunpackopts)
-    #    print js
+    def scan(self, data, url):
         rez = []
+        js = jsunpack.jsunpackn.jsunpack(url, ['', data.encode('utf-8'), url],  self.jsunpackopts )
+
+        for url in js.rooturl:
+            js.rooturl[url].seen = {}
+
+        if js.start in js.rooturl:
+            tmp = js.rooturl[js.start].tostring('', True)[0]
+            print tmp
+            print js.rooturl[js.start].malicious
+            rez.append(tmp)
+            #print tmp
+
         y = self.yara.match(data=self._fil(data))
         if len(y) != 0 and y != "[]":
             print "Detected"
             print y
-        return y
+            rez.append(y)
+        return rez
 
 # mainly for testing
 def ex(url):
     f = crawler.Fetcher(url)
     f.fetch()
     s = Scanner()
-    r = s.scan(f.content)
+    r = s.scan(f.content, f.url)
     print r
 
 
