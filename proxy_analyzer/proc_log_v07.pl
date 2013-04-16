d35ed5c71b0603b0faf2e917771d1407d9b70251
sergey soldatov <votadlos@gmail.com>
2013-04-16 23:35:44 +0400

    See CHANGELOG

--- proxy_analyzer/proc_log_v07.pl
+++ proxy_analyzer/proc_log_v07.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 #
-# v.8 2013-04-02 svsoldatov
+# v.9 2013-04-16 svsoldatov
 #
 
 use lib "/root/perl5/lib/perl5"; #CPAN home
@@ -17,13 +17,13 @@ do "Lists.conf"; # Threashold and Output files of colored lists
 #####################################
 # G L O B A L S 
 #####################################
-$BUF_SIZE = 30000; #lines in buffer
-$MAX_LOAD_AVG = 5; #CPU load average after that we'd better go to sleep (used in --match only)
+$BUF_SIZE = 2000; #lines in buffer
+$MAX_LOAD_AVG = 9; #CPU load average after that we'd better go to sleep (used in --match only)
 $SLEEP = 10; #sleep when CPU overloaded (used in --match only)
 
 @buf = (); #array to store buffer 
 @buf2 = (); #array to store buffer 
-$CHILDREN_NUM = 7; #concurrent worker processes (used in --list only)
+$CHILDREN_NUM = 9; #concurrent worker processes (used in --list only)
 @CHILDREN = (); #(used in --list only)
 #####################################
 
@@ -32,9 +32,6 @@ $CHILDREN_NUM = 7; #concurrent worker processes (used in --list only)
 #
 GetOptions( 
 	convl2iponly => \$convert_l2ip_only, #convert long to ip
-	
-	# Parse already converted file, search only
-	append => \$write_mode_append,
 
 	help => \$help,
 	
@@ -42,7 +39,6 @@ GetOptions(
 	"list=s" => \$list_search, #perform list search (can be list:s, but doesn't work im my env :-(()
 	"match=s" => \$match_search, #perform exact match search
 	printscore => \$printscore, #print score into output file
-	dnconv => \$dnconert, #do not convert (log is already in OUTPUT format)
 );
 
 if($help){
@@ -72,9 +68,6 @@ elsif($list_search){
 
 	my %lists = ();
 	readLists(\%lists);
-	#showLists(\%lists); #DEBUG
-
-	openOutputFiles(\%lists);
 
 	@buf = ();
 	while (get_next_buf_to_parse() > 0){
@@ -99,13 +92,29 @@ elsif($list_search){
 					@CHILDREN = (); #no children
 					$SIG{INT} = \&int_handler;
 		
-					#print "$$: First line in buffer $buf[0] ..\n\n"; #DEBUG
-					#print "$$: Last line in buffer $buf[$#buf] ..\n\n"; #DEBUG
+					#print "$$: First line in buffer ".substr($buf[0],0,50)." ..\n\n"; #DEBUG
+					#print "$$: Last line in buffer ".substr($buf[$#buf],0,50)." ..\n\n"; #DEBUG
+					my $line_counter = 0;
+
 					##################
 					#sleep 1; #DEBUG #
 					#exit 0;  #DEBUG #
 					##################
-
+					
+					# Open output file
+					foreach my $color (keys %ParserCfg::Lists){
+						my $out_file_name = $ParserCfg::Lists{$color}->{OutFile};
+						my $t = time();
+						unless ( ($out_file_name =~ s/PID/$$/) && ($out_file_name =~ s/TIME/$t/) ){
+							my @tok = split /\./, $out_file_name;
+							$tok[$#tok-1] .= "_${t}_$$";
+							$out_file_name = join ".",@tok;
+						}
+						open $ParserCfg::Lists{$color}->{OutFileFD}, ">$out_file_name" 
+							or print STDERR "Open '".$ParserCfg::Lists{$color}->{OutFile}."': $!\n";
+						$ParserCfg::Lists{$color}->{OutFile} = $out_file_name;
+					}
+					
 					foreach my $l (@buf){
 						# prepare line
 						my @f = transform_log_line($l);
@@ -178,47 +187,44 @@ elsif($list_search){
 							
 						#print "$$: Line: $l\nScore: $linescore\n\n\n"; #DEBUG
 						$l = "($linescore) ".$l if $printscore;
+						$line_counter++;
 
 						#print map {"$_\n"} ($ParserCfg::Lists{ExpWhiteList}->{OutFileFD},$ParserCfg::Lists{WhiteList}->{OutFileFD},$ParserCfg::Lists{GrayList}->{OutFileFD},$ParserCfg::Lists{BlackList}->{OutFileFD},$ParserCfg::Lists{ExpBlackList}->{OutFileFD}); #DEBUG
 			
 						$| = 1; #flush 
 						if ( $ParserCfg::Lists{ExpWhiteList}->{OutFileFD} && ($linescore > $ParserCfg::Lists{ExpWhiteList}->{Thr}) ){
 							#print "Line goes to ExpWhiteList\n"; #DEBUG
-							#flock($ParserCfg::Lists{ExpWhiteList}->{OutFileFD},LOCK_EX);
 							print {$ParserCfg::Lists{ExpWhiteList}->{OutFileFD}} "$l\n";
-							#flock($ParserCfg::Lists{ExpWhiteList}->{OutFileFD},LOCK_UN);
-							#$ParserCfg::Lists{ExpWhiteList}->{OutFileLCNT}++;
 						}
 						elsif( $ParserCfg::Lists{WhiteList}->{OutFileFD} && ($ParserCfg::Lists{WhiteList}->{Thr} <= $linescore) && ($linescore <= $ParserCfg::Lists{ExpWhiteList}->{Thr}) ){
 							#print "Line goes to WhiteList\n"; #DEBUG
-							#flock($ParserCfg::Lists{WhiteList}->{OutFileFD},LOCK_EX);
-							print {$ParserCfg::Lists{WhiteList}->{OutFileFD}} "$l\n";
-							#flock($ParserCfg::Lists{WhiteList}->{OutFileFD},LOCK_UN);
-							#$ParserCfg::Lists{WhiteList}->{OutFileLCNT}++;
+							print {$ParserCfg::Lists{WhiteList}->{OutFileFD}} "$$: $l\n";
 						}
 						elsif( $ParserCfg::Lists{GrayList}->{OutFileFD} && ($ParserCfg::Lists{BlackList}->{Thr} < $linescore) && ($linescore < $ParserCfg::Lists{WhiteList}->{Thr}) ){
 							#print "Line goes to GrayList\n"; #DEBUG
-							#flock($ParserCfg::Lists{GrayList}->{OutFileFD},LOCK_EX);
-							print {$ParserCfg::Lists{GrayList}->{OutFileFD}} "$l\n";
-							#flock($ParserCfg::Lists{GrayList}->{OutFileFD},LOCK_UN);
-							#$ParserCfg::Lists{GrayList}->{OutFileLCNT}++;
+							print {$ParserCfg::Lists{GrayList}->{OutFileFD}} "$$: $l\n";
 						}
 						elsif( $ParserCfg::Lists{BlackList}->{OutFileFD} && ($ParserCfg::Lists{ExpBlackList}->{Thr} <= $linescore) && ($linescore <= $ParserCfg::Lists{BlackList}->{Thr}) ){
 							#print "Line goes to BlackList\n"; #DEBUG
-							#flock($ParserCfg::Lists{BlackList}->{OutFileFD},LOCK_EX);
 							print {$ParserCfg::Lists{BlackList}->{OutFileFD}} "$l\n";
-							#flock($ParserCfg::Lists{BlackList}->{OutFileFD},LOCK_UN);
-							#$ParserCfg::Lists{BlackList}->{OutFileLCNT}++;
 						}
 						elsif( $ParserCfg::Lists{ExpBlackList}->{OutFileFD} && ($linescore < $ParserCfg::Lists{ExpBlackList}->{Thr}) ){
 							#print "Line goes to ExpBlackList\n"; #DEBUG
-							#flock($ParserCfg::Lists{ExpBlackList}->{OutFileFD},LOCK_EX);
 							print {$ParserCfg::Lists{ExpBlackList}->{OutFileFD}} "$l\n";
-							#flock($ParserCfg::Lists{ExpBlackList}->{OutFileFD},LOCK_UN);
-							#$ParserCfg::Lists{ExpBlackList}->{OutFileLCNT}++;
+						}
+						else {
+							print STDERR "$l\n"; #DEBUG
 						}
 					}
 					
+					# Close output files
+					map {
+						close $ParserCfg::Lists{$_}->{OutFileFD};
+						unlink $ParserCfg::Lists{$_}->{OutFile} if (-z $ParserCfg::Lists{$_}->{OutFile}); #delete if size is zero
+					} (keys %ParserCfg::Lists);
+
+					#print "$$: end processed $line_counter lines\n"; #DEBUG
+
 					exit 0;
 				} # CHILD body
 			}
@@ -233,9 +239,6 @@ elsif($list_search){
 
 		#print "$$ waits...\n"; #DEBUG
 	}
-
-	# close files
-	closeOutputFiles(\%lists);
 }
 elsif($match_search){
 	print  "Enter match Search --$match_search--\n";#DEBUG
@@ -244,9 +247,6 @@ elsif($match_search){
 	my %lists = ();
 	readLists(\%lists);
 	#showLists(\%lists); #DEBUG
-
-	openOutputFiles(\%lists);
-
 
 	my $h = tie @buf, 'IPC::Shareable', {
 		key => 'GLUE',
@@ -274,51 +274,66 @@ elsif($match_search){
 	print "$$: buf size: ".@{$buf_ref}."\n"; #DEBUG
 	
 	while (@{$buf_ref} > 0){
-
-		foreach my $data (keys %{$lists{MatchSearch}}){
-			#print "fork...\n"; #DEBUG
-			my $pid = fork();
-			if ( $pid  > 0 ){
-				#PAPA
-				push @CHILDREN, $pid;
-
-				$SIG{CHLD} = \&child_handler;
-				$SIG{INT} = \&int_handler;
-			}
-			else {
-				#CHILD
-				@CHILDREN = ();
-				$SIG{INT} = \&int_handler;
-				$SIG{USR1} = \&usr_handler;
-
-				print "Child $$: data='$data' buf size=".@{$buf_ref}."\n";
-				#print "$$: First line in buffer $buf_ref->[0] ..\n\n"; #DEBUG
-				#print "$$: Last line in buffer $buf_ref->[@{$buf_ref}-1] ..\n\n"; #DEBUG
-				#sleep 2;
-				#exit 0;
-
-				#find field indx
-				my $i = 0; #Output line field counter
-				foreach my $fn (@{$ParserCfg::Proclog{Output}}){ #field name
-					if ($fn eq $lists{MatchSearch}->{$data}->{FN}){
-						last;
+		foreach my $fn ( keys %lists ){
+			my $fn_i = indexOf($ParserCfg::Proclog{Output}, $fn);
+			if (defined $fn_i){
+				foreach my $data (keys %{$lists{$fn}->{MatchSearch}}){
+					#print "fork...\n"; #DEBUG
+					my $pid = fork();
+					if ( $pid  > 0 ){
+						#PAPA
+						push @CHILDREN, $pid;
+	
+						$SIG{CHLD} = \&child_handler;
+						$SIG{INT} = \&int_handler;
 					}
-					$i++;
-				}
+					else {
+						#CHILD
+						@CHILDREN = ();
+						$SIG{INT} = \&int_handler;
+						$SIG{USR1} = \&usr_handler;
+
+						print "Child $$: data='$data' field='$fn' ($fn_i) buf size=".@{$buf_ref}."\n"; #DEBUG
+						#print "$$: First line in buffer $buf_ref->[0] ..\n\n"; #DEBUG
+						#print "$$: Last line in buffer $buf_ref->[@{$buf_ref}-1] ..\n\n"; #DEBUG
+						#sleep 2;
+						#exit 0;
+
+						# open file
+						my $out_file_name = $lists{$fn}->{MatchSearch}->{"$data"}->{OFN};
+						my $t = time();
+						unless ( ($out_file_name =~ s/PID/$$/) && ($out_file_name =~ s/TIME/$t/) ){
+							my @tok = split /\./, $out_file_name;
+							$tok[$#tok-1] .= "_${fn}_${t}_$$";
+							$out_file_name = join ".",@tok;
+						}
+						open $lists{$fn}->{MatchSearch}->{$data}->{OFD}, ">$out_file_name"
+							or print STDERR "Open '$out_file_name': $!\n";
+						$lists{$fn}->{MatchSearch}->{"$data"}->{OFN} = $out_file_name;
 				
-				#my $j = 0; #DEBUG
-				foreach my $l (@{$buf_ref}){ #lines
-					my @f = transform_log_line($l);
-					$l = join_log_line(@f);
-					#print "$$: $l\n"; #DEBUG
+						#my $j = 0; #DEBUG
+						foreach my $l (@{$buf_ref}){ #lines
+							my @f = transform_log_line($l);
+							$l = join_log_line(@f);
+							#print "$$: $l\n"; #DEBUG
+							
+							if ($f[$fn_i] =~ /$data/){
+								print {$lists{$fn}->{MatchSearch}->{$data}->{OFD}} "$l\n";
+								#print "MATCH: $l\n"; #DEBUG
+							}
 
-					print {$lists{MatchSearch}->{$data}->{OFD}} "$l\n" if $f[$i] =~ /$data/;
-					#$j++; #DEBUG
-					#print "$$: line ", $j,"\n" if ($j%10000 == 0); #DEBUG
+							#$j++; #DEBUG
+							#print "$$: line ", $j,"\n" if ($j%10000 == 0); #DEBUG
+						}
+
+						close $lists{$fn}->{MatchSearch}->{$data}->{OFD};
+						unlink $lists{$fn}->{MatchSearch}->{"$data"}->{OFN} 
+							if (-z $lists{$fn}->{MatchSearch}->{"$data"}->{OFN});
+	
+						exit 0;
+					} #Child body
 				}
-
-				exit 0;
-			} #Child body
+			}
 		}
 		
 		my $buf_ref2 = get_next_buf_to_parse2();
@@ -344,7 +359,6 @@ elsif($match_search){
 	$h2->shunlock();
 
 	# close files
-	closeOutputFiles(\%lists);
 }
 else{ 
 	printUsage();
@@ -354,6 +368,19 @@ else{
 
 ###################################################
 #  S U B S
+###################################################
+sub indexOf {
+	my ($ar, $f) = @_;	
+
+	my $i = 0;
+	foreach my $fi (@{$ar}){
+		if ($fi eq $f){
+			return $i;
+		}
+		$i++;
+	}
+	return undef;
+}
 ###################################################
 #
 # read lists
@@ -382,8 +409,8 @@ sub readLists {
 				}
 				elsif ($a->{FUNC} eq "MatchSearch"){
 					my ($data,$ofn) = split /##/, $ll;
-					#print "MatchSearch: $data, $ofn\n"; #DEBUG
-					$ref->{$a->{FUNC}}->{$data} = {FN=>$f,OFN=>$ofn};
+					print "MatchSearch: $data, $ofn\n"; #DEBUG
+					$ref->{$f}->{$a->{FUNC}}->{$data}->{OFN} = $ofn;
 				}
 				#print "goes to ${f}::".$a->{FUNC}."\n"; #DEBUG
 			}
@@ -449,7 +476,6 @@ print <<EEE;
 Serach patterns in HTTP logs and store results in different files.
 	OPTIONS:
 	--convl2iponly - convert Long to dotted IP only w/o search
-	--append - write results in append mode
 	--list=<FILE> - list serach with specified config
 	--io=<FILE> - Input/Output config
 	--printscore - print logline score into output file
@@ -466,42 +492,58 @@ sub transform_log_line {
 	my @ret = ();
 	
 	$LINE =~ s/^\s+//; $l =~ s/\s+$//;
-	foreach my $fn ( @{$ParserCfg::Proclog{Output}} ) {
-		my $regexp = $ParserCfg::Proclog{REGEXP};
-		foreach my $ffn ( @{$ParserCfg::Proclog{Output}} ) {
-			if($fn eq $ffn){
-				my $freg = '('.$ParserCfg::Proclog{FIELDS}->{$ffn}.')';
-				$regexp =~ s/$ffn/$freg/;
-			}
-			else {
-				$regexp =~ s/$ffn/$ParserCfg::Proclog{FIELDS}->{$ffn}/;
-			}
-		}
-		if ($LINE =~ /$regexp/){
-			my $fff = $1;
-			if($fn =~ /_IP$/){
-				$fff = long2ip($fff);
-			}
-			push @ret, $fff;
-			#print "OK on $fn ($1)\n\n"; #DEBUG
-		}
-		#else {
-		#	print "ERROR on $fn: '$regexp'\n\n$LINE\n\n"; #DEBUG
-		#}
+	if (defined $ParserCfg::Proclog{DELIMETER}){
+		my @inpfields = split /$ParserCfg::Proclog{DELIMETER}/,$LINE;
+		#print "inpfields: ".join("::",@inpfields)."\n"; #DEBUG
+		@ret = map {$_=~/_IP$/ ? long2ip($inpfields[$ParserCfg::Proclog{FIELDS}->{$_}->{N}]) : $inpfields[$ParserCfg::Proclog{FIELDS}->{$_}->{N}]} @{$ParserCfg::Proclog{Output}};
 	}
+	else {
+		#print "ELSE!\n"; #DEBUG
+		foreach my $fn ( @{$ParserCfg::Proclog{Output}} ) {
+			my $regexp = $ParserCfg::Proclog{REGEXP};
+			foreach my $ffn ( @{$ParserCfg::Proclog{Output}} ) {
+				if($fn eq $ffn){
+					my $freg = '('.$ParserCfg::Proclog{FIELDS}->{$ffn}->{RE}.')';
+					$regexp =~ s/$ffn/$freg/;
+				}
+				else {
+					$regexp =~ s/$ffn/$ParserCfg::Proclog{FIELDS}->{$ffn}->{RE}/;
+				}
+			}
+			if ($LINE =~ /$regexp/){
+				my $fff = $1;
+				if($fn =~ /_IP$/){
+					$fff = long2ip($fff);
+				}
+				push @ret, $fff;
+				#print "OK on $fn ($1)\n\n"; #DEBUG
+			}
+			#else {
+			#	print "ERROR on $fn: '$regexp'\n\n$LINE\n\n"; #DEBUG
+			#}
+		}
+	}
+	#print "transform_log_line: ".join('::',@ret)."\n"; #DEBUG
 	return @ret;
 }
 ###################################################
 sub join_log_line {
 	my @ar = @_;
-	my $l = $ParserCfg::Proclog{OUTPUT};
+	my $l = '';
 
-	my $j = 0;
-	foreach my $fn ( @{$ParserCfg::Proclog{Output}} ) {
-		$l =~ s/$fn/$ar[$j]/;
-		$j++;
+	if ($ParserCfg::Proclog{DELIMETER}){
+		$l = join ($ParserCfg::Proclog{DELIMETER}, @ar);
 	}
-
+	else {
+		$l = $ParserCfg::Proclog{OUTPUT};
+		my $j = 0;
+		foreach my $fn ( @{$ParserCfg::Proclog{Output}} ) {
+			$l =~ s/$fn/$ar[$j]/;
+			$j++;
+		}
+	}
+	
+	#print "join_log_line: $l\n"; #DEBUG
 	return $l;
 }
 ###################################################
@@ -599,48 +641,5 @@ sub get_next_buf_to_parse2 {
 	print "$$: $l_read lines read\n\n"; #DEBUG
 	
 	return $buf_ref;
-}
-######################################################
-# open output files
-######################################################
-sub openOutputFiles {
-	my $ref = shift;
-
-	# open colored files
-	if ($list_search){
-		foreach my $l (keys %ParserCfg::Lists){
-			if ($ParserCfg::Lists{$l}->{OutFile}){
-				undef $ParserCfg::Lists{$l}->{OutFileFD};
-				open ($ParserCfg::Lists{$l}->{OutFileFD}, $write_mode_append ? ">>":">", $ParserCfg::Lists{$l}->{OutFile}) 
-					or die "open ".$ParserCfg::Lists{$l}->{OutFile}.": $!\n";
-			}
-		}
-	}
-	
-	# open write-just-matched files (MatchSearch algorithm)
-	if ($match_search){
-		foreach my $a (keys %{$ref}){ #FUNC
-			foreach my $data (keys %{$ref->{$a}}){ #FUNC
-				open($ref->{$a}->{$data}->{OFD},$write_mode_append ? ">>":">", $ref->{$a}->{$data}->{OFN})
-					or die "Open ".$ref->{$a}->{$data}->{OFN}.": $!\n";
-			}
-		}
-	}
-}
-sub closeOutputFiles {
-	my $ref = shift;
-
-	# close colored lists
-	map {
-		close $ParserCfg::Lists{$_}->{OutFileFD}; 
-		unlink $ParserCfg::Lists{$_}->{OutFile} if (-z $ParserCfg::Lists{$_}->{OutFile}); #delete if size is zero
-	} (keys %ParserCfg::Lists) if $list_search;
-	
-	# close write-just-matched files (MatchSearch algorithm)
-	# $ref->{$a->{FUNC}}->{$data} = {FN=>$f,OFN=>$ofn};
-	map {
-		close($ref->{MatchSearch}->{$_}->{OFD});
-		unlink $ref->{MatchSearch}->{$_}->{OFN} if (-z $ref->{MatchSearch}->{$_}->{OFN}); #delete if zerosize
-	} (keys %{$ref->{MatchSearch}}) if $match_search;
 }
 
